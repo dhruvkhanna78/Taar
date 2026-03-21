48f0e1be16b951b256f08a4d374640f790f8bc95
Dhruv Khanna <145452106+dhruvkhanna78@users.noreply.github.com>
2026-03-21 22:56:51 +0530

    fixes for deployment

--- backend/services/emailServices.js
+++ backend/services/emailServices.js
@@ -1,28 +1,27 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    service : 'gmail',
-    auth : {
-        user : process.env.EMAIL_USER,
-        pass : process.env.EMAIL_PASS
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     }
 });
 
 export const sendOTPEmail = async (email, otp) => {
-    try{
+    try {
         const info = await transporter.sendMail({
-            from : process.env.EMAIL_USER,
-            to : email,
-            subject : 'Your OTP',
-            text : `Your OTP is ${otp}. It will expire in 5 minutes.`
-        })
-
-        console.log("Email sent: " + info.response);
-    } catch(error){
-        console.log(error); 
-        return res.status(500).json({
-            message: "Server error, please try again",
-            success: false
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: "Your OTP",
+            text: `Your OTP is ${otp}. It will expire in 5 minutes.`,
         });
+
+        console.log("Email sent:", info.response);
+    } catch (error) {
+        console.log("Email sending failed:", error);
+        throw error; // controller handle karega error
     }
-}
+};
