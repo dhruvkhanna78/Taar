0f0795785af0b69ed180a7adb0f8c3f23fef8dea
Dhruv Khanna <145452106+dhruvkhanna78@users.noreply.github.com>
2026-04-08 22:39:01 +0530

    OTP system

--- backend/services/emailServices.js
+++ backend/services/emailServices.js
@@ -1,22 +1,22 @@
-// import { Resend } from "resend";
+import { Resend } from "resend";
 
-// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-// export const sendOTPEmail = async (email, otp) => {
-//   try {
-//     const data = await resend.emails.send({
-//       from: "Taar <onboarding@resend.dev>",
-//       to: email,
-//       subject: "Your OTP",
-//       html: `<p>Your OTP is <strong>${otp}</strong>. It expires in 5 minutes.</p>`,
-//     });
+export const sendOTPEmail = async (email, otp) => {
+  try {
+    const data = await resend.emails.send({
+      from: "Taar <onboarding@resend.dev>",
+      to: email,
+      subject: "Your OTP",
+      html: `<p>Your OTP is <strong>${otp}</strong>. It expires in 5 minutes.</p>`,
+    });
 
-//     console.log("Email sent:", data);
-//   } catch (error) {
-//     console.error("Resend error:", error);
-//     throw new Error("Email sending failed");
-//   }
-// };
+    console.log("Email sent:", data);
+  } catch (error) {
+    console.error("Resend error:", error);
+    throw new Error("Email sending failed");
+  }
+};
 
 import nodemailer from "nodemailer";
 
@@ -31,19 +31,19 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-export const sendOTPEmail = async (email, otp) => {
-    console.log("📨 Sending OTP to:", email);
-  try {
-    const info = await transporter.sendMail({
-      from: `"Taar" <${process.env.EMAIL_USER}>`,
-      to: email,
-      subject: "Your OTP",
-      text: `Your OTP is ${otp}. It will expire in 5 minutes.`,
-    });
+// export const sendOTPEmail = async (email, otp) => {
+//     console.log("📨 Sending OTP to:", email);
+//   try {
+//     const info = await transporter.sendMail({
+//       from: `"Taar" <${process.env.EMAIL_USER}>`,
+//       to: email,
+//       subject: "Your OTP",
+//       text: `Your OTP is ${otp}. It will expire in 5 minutes.`,
+//     });
 
-    console.log("Email sent: " + info.response);
-  } catch (error) {
-    console.log(error);
-    throw new Error("Email sending failed");
-  }
-};
+//     console.log("Email sent: " + info.response);
+//   } catch (error) {
+//     console.log(error);
+//     throw new Error("Email sending failed");
+//   }
+// };
