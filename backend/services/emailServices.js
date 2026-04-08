6288a74600fad91299818a525d70b018f6b03e37
Dhruv Khanna <145452106+dhruvkhanna78@users.noreply.github.com>
2026-04-09 05:15:35 +0530

    responsiveness

--- backend/services/emailServices.js
+++ backend/services/emailServices.js
@@ -21,57 +21,57 @@
 
 //WORKING CODE FOR BREVO EMAIL SERVICE
 
-// import nodemailer from "nodemailer";
+import nodemailer from "nodemailer";
 
-// const transporter = nodemailer.createTransport({
-//   host: "smtp-relay.brevo.com",
-//   port: 2525,
-//   secure: false,
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS,
-//   },
-// });
-
-// export const sendOTPEmail = async (email, otp) => {
-//   console.log("📨 Sending OTP to:", email);
-
-//   const info = await transporter.sendMail({
-//     from: `"Taar" <${process.env.BREVO_USER}>`,
-//     to: email,
-//     subject: "Your OTP",
-//     text: `Your OTP is ${otp}`,
-//   });
-
-//   console.log("Email sent:", info.response);
-// }
-
-
-//WORKING CODE FOR BREVO EMAIL SERVICE USING API
-
-import axios from "axios";
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 export const sendOTPEmail = async (email, otp) => {
   console.log("📨 Sending OTP to:", email);
 
-  const response = await axios.post(
-    "https://api.brevo.com/v3/smtp/email",
-    {
-      sender: {
-        name: "Taar",
-        email: "khannadhruv242@gmail.com",
-      },
-      to: [{ email }],
-      subject: "Your OTP",
-      textContent: `Your OTP is ${otp}`,
-    },
-    {
-      headers: {
-        "api-key": process.env.EMAIL_PASS,
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const info = await transporter.sendMail({
+    from: `"Taar" <${process.env.BREVO_USER}>`,
+    to: email,
+    subject: "Your OTP",
+    text: `Your OTP is ${otp}`,
+  });
 
-  console.log("Email sent:", response.data);
-};
+  console.log("Email sent:", info.response);
+}
+
+
+//WORKING CODE FOR BREVO EMAIL SERVICE USING API
+
+// import axios from "axios";
+
+// export const sendOTPEmail = async (email, otp) => {
+//   console.log("📨 Sending OTP to:", email);
+
+//   const response = await axios.post(
+//     "https://api.brevo.com/v3/smtp/email",
+//     {
+//       sender: {
+//         name: "Taar",
+//         email: "khannadhruv242@gmail.com",
+//       },
+//       to: [{ email }],
+//       subject: "Your OTP",
+//       textContent: `Your OTP is ${otp}`,
+//     },
+//     {
+//       headers: {
+//         "api-key": process.env.EMAIL_PASS,
+//         "Content-Type": "application/json",
+//       },
+//     }
+//   );
+
+//   console.log("Email sent:", response.data);
+// };
