affa3348bcb6dfe5a585b600f45e51f77529d25e
Dhruv Khanna <145452106+dhruvkhanna78@users.noreply.github.com>
2026-05-02 00:57:46 +0530

    Notification

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,7 +6,7 @@ const onlineUsers = new Map();
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173"
+      origin: "https://taar-szo1.onrender.com"
     }
   });
 
--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000");
+const socket = io("https://taar-server.onrender.com");
 
 export default socket;
